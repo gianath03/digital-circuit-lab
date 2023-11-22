dd9816ff029c3097c99079e83fb42e27377584dd
Giannis Athanasiadis <128140023+gianath03@users.noreply.github.com>
2023-11-22 15:38:13 +0200

    project_2: started baud_controller.v

---
+++ project_2/project_2.srcs/sim_1/new/baud_controller_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+`define clock_period 10
+
+module baud_controller_tb;
+    reg clk, reset;
+    reg [2:0] baud_select;
+    wire sample_ENABLE;
+
+    baud_controller baud_controller_inst (.reset(reset), .clk(clk), .baud_select(baud_select), .sample_ENABLE(sample_ENABLE));
+
+    initial begin
+        clk = 1'b0; reset = 1'b0; baud_select = 3'b000;
+        #200 reset = 1'b1;
+        #400 reset = 1'b0;
+    end
+
+    always begin
+        #(`clock_period/2) clk = ~clk;
+    end
+endmodule
