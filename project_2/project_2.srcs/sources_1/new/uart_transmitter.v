103a7c141e65e43642e0d5553d78f2307ea0c9af
gianath03 <128140023+gianath03@users.noreply.github.com>
2023-11-25 12:04:32 +0200

    Project_2: Added sync to the reset button of partB

--- project_2/project_2.srcs/sources_1/new/uart_transmitter.v
+++ project_2/project_2.srcs/sources_1/new/uart_transmitter.v
@@ -6,18 +6,23 @@ module uart_transmitter(reset, clk, Tx_DATA, baud_select, Tx_WR, Tx_EN, TxD, Tx_
     input Tx_WR;
     output reg TxD;
     output reg Tx_BUSY;
+    
     reg [7:0] data;
+    wire reset_clean;
+
+    //Sync reset to the clock.
+    sync_reest_module sync_reest (.clk(clk), .reset(reset), .reset_clean(reset_clean));
 
     //Read data from system.
-    transmitter_WR_module transmitter_WR (.clk(clk), .reset(reset), .data(data), .Tx_DATA(Tx_DATA), .Tx_BUSY(Tx_BUSY));
+    transmitter_WR_module transmitter_WR (.clk(clk), .reset(reset_clean), .data(data), .Tx_DATA(Tx_DATA), .Tx_BUSY(Tx_BUSY));
 
     //Set baud rate to Tx_sample_ENABLE /16 speed.
-    trasmitter_baud trasmitter_baud_inst (.baud_tick(baud_tick), .reset(reset), .Tx_sample_ENABLE(Tx_sample_ENABLE));
+    trasmitter_baud trasmitter_baud_inst (.baud_tick(baud_tick), .reset(reset_clean), .Tx_sample_ENABLE(Tx_sample_ENABLE));
 
     //Trasmit module when Tx_EN until Tx_BUSY is 0.
-    transmit_module transmit_module_inst (.reset(reset), .Tx_EN(Tx_EN), .Tx_BUSY(Tx_BUSY), .data(data), .baud_tick(baud_tick), .TxD(TxD));
+    transmit_module transmit_module_inst (.reset(reset_clean), .Tx_EN(Tx_EN), .Tx_BUSY(Tx_BUSY), .data(data), .baud_tick(baud_tick), .TxD(TxD));
 
     //Module that creates the correct baud rate depending on the baud_select.
-    baud_controller baud_controller_tx_inst(reset, clk, baud_select, Tx_sample_ENABLE);
+    baud_controller baud_controller_tx_inst(reset_clean, clk, baud_select, Tx_sample_ENABLE);
 
 endmodule
