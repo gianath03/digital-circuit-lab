cda847ffb78df2aafca9e7f6cd8ec6e41e2fd084
Giannis Athanasiadis <128140023+gianath03@users.noreply.github.com>
2023-12-27 21:47:28 +0200

    Project_3: Updated display_enable

--- project_3/project_3.srcs/sources_1/new/sync_pulse_module.v
+++ project_3/project_3.srcs/sources_1/new/sync_pulse_module.v
@@ -6,13 +6,13 @@ module sync_pulse_module(
     output [13:0]           adr,
     output                hsync,
     output                vsync,
-    output        display_vtime,
-    output        display_htime);
+    output         display_time);
 
     wire [6:0] hpixel;
     wire [6:0] vpixel;
 
     assign adr = {vpixel, hpixel};
+    assign display_time = (display_htime && display_vtime);
 
     hpixel_module hpixel_module_inst (.clk(clk), .reset(reset), .hsync(hsync), .hpixel(hpixel), .display_time(display_htime));
     vsync_module vsync_module_inst (.clk(clk), .reset(reset), .hsync(hsync), .vsync(vsync), .vpixel(vpixel), .display_time(display_vtime));
